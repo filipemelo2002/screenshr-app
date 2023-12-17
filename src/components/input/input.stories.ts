baa79a675a3924f64dc7dff0923693a279cc85b7
Filipe Melo <filipe.silva@encora.com>
2023-12-17 16:43:11 -0300

    feature: create input element

---
+++ src/components/input/input.stories.ts
@@ -0,0 +1,21 @@
+import { Input } from "./input";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Input> = {
+  title: 'ScreenShr/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs'],
+}
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Primary: Story = {
+  args: {
+    
+  }
+};
