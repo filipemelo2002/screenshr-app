f9e841e0c350d2f3b5389fb5441426676a4af059
Filipe Melo <filipe.silva@encora.com>
2023-12-29 15:03:20 -0300

    feature: create JoinParty screen

--- src/app/join-party/page.tsx
+++ src/app/join-party/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+import { Button } from "@/components/button/button";
+import { ColorPicker } from "@/components/color-picker/color-picker";
+import { Input } from "@/components/input/input";
+import { Layout } from "@/components/layout/layout";
+
 export default function JoinParty() {
-  return <h1>Screenshr, Join Party</h1>;
+  return (
+    <Layout navigateBack={() => {}}>
+      <form className="mt-[41px]">
+        <div className="flex flex-col">
+          <label className="font-bold font-sans text-sm text-steel-gray mb-3">
+            Party code
+          </label>
+          <div className="flex gap-4 max-w-[300px] text-center font-bold">
+            <Input className="text-2xl text-center uppercase" maxLength={1} />
+            <Input className="text-2xl text-center uppercase" maxLength={1} />
+            <Input className="text-2xl text-center uppercase" maxLength={1} />
+            <Input className="text-2xl text-center uppercase" maxLength={1} />
+            <Input className="text-2xl text-center uppercase" maxLength={1} />
+          </div>
+          <div className="mt-4">
+            <Input label="Nickname" />
+          </div>
+          <ColorPicker
+            value="bg-pink"
+            onChange={() => {}}
+            label="Profile color"
+            className="mt-4"
+          />
+          <Button className="mt-4" primary>
+            Join party
+          </Button>
+        </div>
+      </form>
+    </Layout>
+  );
 }
