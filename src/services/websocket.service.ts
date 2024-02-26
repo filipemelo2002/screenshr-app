1a2b81c49cf509fbb3c2aa0b806b22efe4259249
Filipe Melo <filipe.silva@encora.com>
2024-02-26 10:04:13 -0300

    feature: make websocket instance singleton

--- src/services/websocket.service.ts
+++ src/services/websocket.service.ts
@@ -21,22 +21,19 @@ interface JoinRoomRequest {
   color: string;
   roomId: string;
 }
+
+const websocket = io("http://localhost:3000/", {
+  transports: ["websocket"],
+});
+
 export class WebsocketService {
-  private websocket: Socket;
-
-  constructor() {
-    this.websocket = io("http://localhost:3000/", {
-      transports: ["websocket"],
-    });
-  }
-
   async createRoom({ nickname, color }: CreateRoomRequest): Promise<Room> {
     const response = await fetch("http://localhost:3000/rooms", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ nickname, color, owner: this.websocket.id }),
+      body: JSON.stringify({ nickname, color, owner: websocket.id }),
     });
 
     const data = await response.json();
@@ -49,7 +46,7 @@ export class WebsocketService {
   }
 
   async joinRoom({ nickname, color, roomId }: JoinRoomRequest) {
-    this.websocket.emit(EVENTS.JOIN_ROOM, {
+    websocket.emit(EVENTS.JOIN_ROOM, {
       nickname,
       color,
       roomId,
