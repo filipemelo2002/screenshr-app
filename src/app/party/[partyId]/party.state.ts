190e7edaacf4f41e3b947fa690b46d6b808fb9e3
Filipe Melo <filipe.silva@encora.com>
2024-02-26 10:51:07 -0300

    feature: prevent user to join without id or nickname

--- src/app/party/[partyId]/party.state.ts
+++ src/app/party/[partyId]/party.state.ts
@@ -1,14 +1,23 @@
+import { useRoomStore } from "@/zustand/room.store";
 import { useUserStore } from "@/zustand/user.store";
 import { useRouter } from "next/navigation";
+import { useLayoutEffect } from "react";
 
 export const useParty = () => {
   const router = useRouter();
 
   const { nickname, color, id, isStreaming } = useUserStore();
+  const { id: roomId } = useRoomStore();
 
   const navigateBack = () => {
     router.push("/");
   };
+
+  useLayoutEffect(() => {
+    if (!nickname || !roomId) {
+      router.push("/join-party");
+    }
+  }, [nickname, roomId, router]);
 
   return {
     navigateBack,
